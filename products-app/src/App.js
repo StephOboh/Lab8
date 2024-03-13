09078c2f5694688d9a8790ba412f330e8bebe82b
StephO <stephenie.oboh@ucalgary.ca>
2024-03-13 16:42:45 -0600

    step B

--- products-app/src/App.js
+++ products-app/src/App.js
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import { ProductsProvider } from './context/ProductsContext'; // Adjust the import path as necessary
